a7605e25f14aaef1a60a5c9858281703ee8457fc
lee <707678047@qq.com>
2020-02-02 21:04:13 +0800

    update API

--- js/jquery-weather.js
+++ js/jquery-weather.js
@@ -7,21 +7,22 @@
         };
         var s=this;
         var ops = $.extend(defaults, options);
-        var u = "https://www.tianqiapi.com/api?city=" + ops.city;
+        var u = "https://tianqiapi.com/api?version=v6&appid=94296478&appsecret=bY9OnPbj&city=" + ops.city;
         $.ajax({
             url: u,
-            dataType: "jsonp",
-            jsonpCallback:"callback",
+            //dataType: "jsonp",
+            //jsonpCallback:"callback",
             success:function (data) {
                methods.render(s,data,ops)
+			   
             }
         });
     }
 
     var methods={
-        render:function (s,data,ops) {
+        render:function (s,data,ops) {		
             let air_level="";
-            let air=data.data[0].air;
+            let air=data.air;
             if(air<=50){
                 air_level="btn-info"
             }
@@ -37,22 +38,22 @@
 
             html=`<div class="row">
                      <p>${data.city}</p>
-                     <p>${data.data[0].date+" "+data.data[0].week}</p>
-                     <p><img src="https://cdn.huyahaha.com/tianqiapi/skin/${ops.skin}/${data.data[0].wea_img}.png"></p>
+                     <p>${data.date+" "+data.week}</p>
+                     <p><img src="https://www.tianqiapi.com/static/skin/${ops.skin}/${data.wea_img}.png"></p>
                      <div class="tem row">
                       <div class="col-sm-6 col-xs-6">
-                       <p>${data.data[0].tem.substring(0,data.data[0].tem.length-1)}</p>
+                       <p>${data.tem}</p>
                      </div>
                     <div class="col-sm-6 col-xs-6 ">
                         <p>℃</p>
-                        <p>${data.data[0].wea}</p>
+                        <p>${data.wea}</p>
                     </div>
                     </div>
-                    <p>${data.data[0].tem2+"~"+data.data[0].tem1}</p>
-                    <p>${data.data[0].win[0]}</p>
-                    <p class="${air_level} air_level">${air+" "+data.data[0].air_level}</p>
+                    <p>${data.tem2+"~"+data.tem1+"℃"}</p>
+                    <p>${data.win}</p>
+                    <p class="${air_level} air_level">${air+" "+data.air_level}</p>
                     <div class="tips ">
-                       ${"tips:"+data.data[0].air_tips}
+                       ${"tips:"+data.air_tips}
                        <span class="bot"></span>
                        <span class="top"></span> 
                     </div>
